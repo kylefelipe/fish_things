5861f2d309c87df5e79f1699306567c71a011233
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2022-07-22 19:00:24 -0300

    modifica o install.fish

--- install_fish.sh
+++ install_fish.sh
@@ -3,7 +3,7 @@
 for i in `ls ./functions/*.fish`
 do
   echo "Copiando arquivo $i para ~/.config/fish/functions/"
-  cp $i ~/.config/fish/functions/
+  ln $i ~/.config/fish/functions/
 done
 
 echo "Reinicie o terminal para que as funções funcionen."
