d9f46d756e489121c2c52ebbb6883abfca135404
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2023-06-20 09:15:15 -0300

    corrigindo bug

--- functions/activate.fish
+++ functions/activate.fish
@@ -1,11 +1,15 @@
 function activate
-    echo "Ativando Ambiente Virtual"
     set QT_ARGS (count $argv)
-    if contains -- -h $argv; or contains -- --help
+    set venv_name ".venv"
+    if contains -- -h $argv; or contains -- --help $argv
         usage
-    else if test $QT_ARGS = 0; or contains -- -p $argv; or contains -- --python $argv
-        set venv_name ".venv"
-        if test $QT_ARGS -ge 2
+        return 1
+    else if test $QT_ARGS = 1
+        echo "Ativando python direto"
+        set-python-venv $argv[1]
+        return 1
+    else if test $QT_ARGS -ge 1; or contains -- -p $argv; or contains -- --python $argv
+        if test $QT_ARGS -ge 1
             set venv_name $argv[2]
         end
         set-python-venv $venv_name
@@ -13,28 +17,28 @@ function activate
 end
 
 function set-python-venv
+    echo "Ativando Ambiente Virtual2"
     set venv_name $argv[1]
     if test -d (pwd)/$venv_name
-        echo "Ambiente Virtual " $venv_name " encontrado"
+        echo "Ambiente Virtual" $venv_name "encontrado"
         source (pwd)/$venv_name/bin/activate.fish
         echo Activated
     else
-        echo "Ambiente virtual "$venv_name" não encontrado"
+        echo "Ambiente virtual" $venv_name "não encontrado"
         echo "Verifique se o ambiente virtual existe ou crie um novo"
     end
 end
 
 function usage
     echo 'Modo de Uso'
-    echo ' activate [-h|--help] [-p|--python] [nome_ambiente_virtual]'
-    echo 'Exemplo:'
-    echo ' activate -p .venv3.10'
-    echo 'Ativa o python do ambiente virtual na pasta .venv3.10'
-    echo 'Parâmetros:'
+    echo ' activate [OPÇÕES] [nome_ambiente_virtual]'
+    echo 'Opções:'
     echo ' -h, --help: Mostra a ajuda'
-    echo ' -p, --python: Não Implementado'
     echo ' nome_ambiente_virtual: Nome da pasta contendo o ambiente virtual a ser ativado,'
     echo '      caso não seja informado, o nome padrão é .venv'
+    echo 'Exemplo:'
+    echo ' activate .venv3.10'
+    echo 'Ativa o python do ambiente virtual na pasta .venv3.10'
 
     return 1
 end
